85814e1cad91c5e721e025f34e1c224f92aebe0f
UP210356 <up210356@alumnos.upa.edu.mx>
2022-09-27 12:19:51 -0500

    27/SEP

---
+++ U1/Edades.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int e;
+   cout <<"Dame tu edad";
+   cin >> e;
+
+    if (1<=e<=150)
+    {
+        if (1<=e<=30)
+        {
+            cout << "Primera Edad";
+            /*else(61<=e<=90)
+            {
+
+            }
+*/
+            
+            
+        }
+        
+    }
+    
+}
