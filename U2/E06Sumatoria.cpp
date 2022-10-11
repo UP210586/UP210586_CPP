f5e325934e365b04119d19af2e6310b116f549b5
UP210356 <up210356@alumnos.upa.edu.mx>
2022-10-11 13:29:25 -0500

    sum

---
+++ U2/E06Sumatoria.cpp
@@ -0,0 +1,21 @@
+//Objetivo: Sumatoria
+//          Debug
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int s, n;
+    s=0;
+    n=1;
+    while (n<=10)
+    {
+        s=s+n;
+        n=n+1;
+    }
+    cout<<"La sumatoria es "<<s<<endl;
+
+    
+    
+    return 0;
+}
